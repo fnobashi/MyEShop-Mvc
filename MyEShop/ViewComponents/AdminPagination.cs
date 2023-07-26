5b70355c265101aa09b60755b82d8b785932d1ed
fnobashi <fnobashi@gmail.com>
2023-07-26 13:56:41 +0330

    Added Admin Pagination ViewComponent

---
+++ MyEShop/ViewComponents/AdminPagination.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEShop.ViewComponents
+{
+    public class AdminPagination:ViewComponent
+    {
+        public IViewComponentResult Invoke(int page , int pageSize ,int totalPages)
+        {
+            ViewBag.TotalPages = totalPages;
+            ViewBag.Page = page;
+            ViewBag.PageSize = pageSize;
+            return View("AdminPagination");
+        }
+    }
+}
