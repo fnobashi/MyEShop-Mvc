30d9c1e725cd6ed49d6139a17c5c3f923e07c1fc
fnobashi <fnobashi@gmail.com>
2023-07-26 13:57:19 +0330

    Added GenericRepository

---
+++ MyEShop/Contracts/Repositories/IBaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyEShop.Models;
+using MyEShop.Models.Entities;
+
+namespace MyEShop.Contracts.Repositories
+{
+    public interface IBaseRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        #region Methods
+
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(TKey id);
+
+        void Add(TEntity entity);
+
+        void Delete(TKey id);
+
+        void Update(TEntity entity);
+
+        #endregion Methods
+
+        #region AsyncMethods
+
+        Task<TEntity> GetByIdAsync(TKey id);
+
+        Task AddAsync(TEntity entity);
+
+        Task DeleteAsync(TKey id);
+
+        Task UpdateAsync(TEntity entity);
+
+        #endregion AsyncMethods
+
+        IQueryable<TEntity> GetAll(SortBy sort);
+        void Save();
+        Task SaveAsync();
+
+    }
+}
---
+++ MyEShop/Repositories/GenericRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEShop.Contracts.Repositories;
+using MyEShop.Models;
+using MyEShop.Models.Entities;
+
+namespace MyEShop.Repositories
+{
+    public abstract class GenericRepository<TEntity, Tkey> : IBaseRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        private readonly DbSet<TEntity> dbset;
+        private readonly DbContext context;
+        public GenericRepository(DbContext context)
+        {
+            this.context = context;
+            dbset = context.Set<TEntity>();
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public virtual void Delete(Tkey id)
+        {
+            var entity = dbset.Find(id);
+            entity.DeletedDate = DateTime.Now;
+            entity.IsDeleted = true;
+            context.SaveChanges();
+        }
+
+        public virtual async Task DeleteAsync(Tkey id)
+        {
+            var entity = await dbset.FindAsync(id);
+            entity.DeletedDate = DateTime.Now;
+            entity.IsDeleted = true;
+            await context.SaveChangesAsync();
+        }
+
+        public IQueryable<TEntity> GetAll()
+            => dbset;
+
+
+        public virtual TEntity GetById(Tkey id)
+            => dbset.Find(id);
+
+        public virtual async Task<TEntity> GetByIdAsync(Tkey id)
+            => await dbset.FindAsync(id);
+
+        public virtual void Update(TEntity entity)
+        {
+            entity.ModifiedDate = DateTime.Now;
+            context.Update(entity);
+            context.SaveChanges();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            entity.ModifiedDate = DateTime.Now;
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+        public abstract IQueryable<TEntity> GetAll(SortBy sort);
+
+
+
+    }
+}
