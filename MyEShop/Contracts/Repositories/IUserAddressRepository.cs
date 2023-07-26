429206515cf31d39d6354178f622fc06b15cd3ed
fnobashi <fnobashi@gmail.com>
2023-07-26 14:01:59 +0330

    Added User Repositories

---
+++ MyEShop/Contracts/Repositories/IUserAddressRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyEShop.Models.Entities.Users;
+
+namespace MyEShop.Contracts.Repositories
+{
+    public interface IUserAddressRepository : IBaseRepository<Address, long>
+    {
+        IQueryable<Address> GetAddressesByUserId(Guid id);
+    }
+}
---
+++ MyEShop/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyEShop.Models.Entities.Users;
+
+namespace MyEShop.Contracts.Repositories
+{
+    public interface IUserRepository : IBaseRepository<User, Guid>
+    {
+        string GetPassword(Guid id);
+
+        void ChangePassword(Guid id, string password);
+
+        bool IsEmailInUse(string email);
+
+        bool IsPhoneNumberInUse(string phoneNumber);
+
+        bool IsUserExist(Guid id);
+    }
+}
